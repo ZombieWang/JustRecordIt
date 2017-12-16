600fb3da938f30b436be4c8f76c79947d2cd811e
Frank <frank7300228@gmail.com>
2017-12-16 12:13:17 +0800

    Add data model

---
+++ JustRecordIt/Model/VoiceMemo+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  VoiceMemo+CoreDataClass.swift
+//  JustRecordIt
+//
+//  Created by Frank on 2017/12/16.
+//  Copyright © 2017 Frank. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(VoiceMemo)
+public class VoiceMemo: NSManagedObject {
+    
+}

733dfb2427aab99aac174521d121048a012eeacf
Frank <frank7300228@gmail.com>
2017-12-16 20:01:25 +0800

    Remove old school CoreData syntax, add func saveData(savedDateTime:)

--- JustRecordIt/ViewModel/RecordViewModel.swift
+++ JustRecordIt/ViewModel/RecordViewModel.swift
@@ -7,19 +7,28 @@
 //
 
 import Foundation
+import UIKit
 import AVFoundation
+import CoreData
 
 class RecordViewModel {
+    private let appDelegate: AppDelegate
+    private let context: NSManagedObjectContext
     private var viewController: RecordViewController!
     var audioRecorder: AVAudioRecorder!
     var audioPlayer: AVAudioPlayer!
+    var lastRestoredDateTime: Date?
     
     init(viewController: RecordViewController) {
         self.viewController = viewController
+        
+        appDelegate = UIApplication.shared.delegate as! AppDelegate
+        context = appDelegate.persistentContainer.viewContext
     }
     
     func setupRecorder() {
-        let fileURL = getURLforAudio()
+        lastRestoredDateTime = Date()
+        let fileURL = getURLforAudio(dateTime: lastRestoredDateTime!)
         
         let recordSettings: [String : Any] = [
             AVFormatIDKey: Int(kAudioFormatLinearPCM),
@@ -44,19 +53,25 @@ class RecordViewModel {
     
     func stopRecording() {
         audioRecorder.stop()
+        saveData(savedDateTime: lastRestoredDateTime!)
     }
     
     // MARK: Playback
     func play() {
-        let fileURL = getURLforAudio()
-        do {
-            audioPlayer = try AVAudioPlayer(contentsOf: fileURL)
-            audioPlayer.delegate = viewController
-            if audioPlayer.duration > 0.0 {
-                audioPlayer.play()
+        if let lastSavedDateTime = lastRestoredDateTime {
+            let fileURL = getURLforAudio(dateTime: lastSavedDateTime)
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: fileURL)
+                audioPlayer.delegate = viewController
+                
+                if audioPlayer.duration > 0.0 {
+                    audioPlayer.play()
+                }
+            } catch {
+                print("Error loading audioPlayer.")
             }
-        } catch {
-            print("Error loading audioPlayer.")
+        } else {
+            print("No saved voice memo.")
         }
     }
     
@@ -64,7 +79,32 @@ class RecordViewModel {
         audioPlayer.stop()
     }
     
-    private func getURLforAudio() -> URL {
-        return URL(fileURLWithPath: NSHomeDirectory() + "/Documents/TempMemo.caf")
+    // MARK: Save to CoreData
+    private func saveData(savedDateTime: Date) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:00"
+        
+        let dateString = dateFormatter.string(from: savedDateTime)
+        
+        let newRecord = NSEntityDescription.insertNewObject(forEntityName: "VoiceMemo", into: context)
+        newRecord.setValue(savedDateTime, forKey: "date")
+        newRecord.setValue(dateString, forKey: "name")
+        newRecord.setValue("\(dateString).caf", forKey: "path")
+        
+        do {
+            try context.save()
+            print("VoiceMemo saved.")
+        } catch {
+            print("There was an error.")
+        }
+    }
+    
+    private func getURLforAudio(dateTime: Date) -> URL {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:00"
+        
+        let dateString = dateFormatter.string(from: dateTime)
+        
+        return URL(fileURLWithPath: NSHomeDirectory() + "/Documents/\(dateString).caf")
     }
 }
